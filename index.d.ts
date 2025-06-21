10932dd433db1f1f1d14f6790b71c15e1ded32a8
phothinmg <phothinmg@disroot.org>
2025-06-22 02:37:00 +0630

    modified:   index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,147 @@
-import type {
-  Comment as IComment,
-  Doctype as IDoctype,
-  Element as IElement,
-  ElementContent as IElementContent,
-  Node as INode,
-  Root as IRoot,
-  Text as IText,
-} from "hast";
+export type Attributes = Record<string, string>;
+export type ElementNode = [string, ...(Attributes | JsonMLNode)[]];
+export type JsonMLNode = string | ElementNode;
+export type JsonMLNodes = JsonMLNode[];
+export type JsonMLTree = JsonMLNode[];
 
-export interface Root extends IRoot {}
-export interface Doctype extends IDoctype {}
-export type ElementContent = IElementContent;
-export interface Node extends INode {}
-export interface Element extends IElement {}
-export interface Text extends IText {}
-export interface Comment extends IComment {}
-
-export type TextileVisitor = (
-  node: Root | Doctype | ElementContent,
+export type Option = {
+  breaks?: boolean;
+};
+export type TxastTextNode = {
+  type: "text";
+  value: string;
+};
+export type TxastBreaksNode = {
+  type: "breaks";
+  value: string;
+};
+export type TxastElementNode = {
+  type: "element";
+  tagName: Tags;
+  properties?: Record<string, any>;
+  children?: TxastNodes;
+};
+export type TxastNode = TxastTextNode | TxastBreaksNode | TxastElementNode;
+export type TxastNodes = TxastNode[];
+export type TxastTree = {
+  type: "root";
+  tagName: "html";
+  children: TxastNodes;
+};
+export type WalkFunction = (
+  node: TxastNode,
   index?: number,
-  parent?: Root | Element
+  parent?: TxastNodes
 ) => any;
-export interface TextileTreeWalker {
-  walk: TextileVisitor;
+//
+export interface TextileExtension {
+  walkTree: WalkFunction;
 }
-export interface TextileExtension extends TextileTreeWalker {}
 export type TextileExtensionFn = (...args: any[]) => TextileExtension;
+export type Tags =
+  | "a"
+  | "abbr"
+  | "address"
+  | "area"
+  | "article"
+  | "aside"
+  | "audio"
+  | "b"
+  | "bdi"
+  | "bdo"
+  | "blockquote"
+  | "body"
+  | "br"
+  | "button"
+  | "canvas"
+  | "caption"
+  | "cite"
+  | "code"
+  | "col"
+  | "colgroup"
+  | "data"
+  | "datalist"
+  | "dd"
+  | "del"
+  | "details"
+  | "dfn"
+  | "dialog"
+  | "div"
+  | "dl"
+  | "dt"
+  | "em"
+  | "embed"
+  | "fieldset"
+  | "figcaption"
+  | "figure"
+  | "footer"
+  | "form"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
+  | "header"
+  | "hgroup"
+  | "hr"
+  | "i"
+  | "iframe"
+  | "img"
+  | "input"
+  | "ins"
+  | "kbd"
+  | "label"
+  | "legend"
+  | "li"
+  | "main"
+  | "map"
+  | "mark"
+  | "menu"
+  | "meter"
+  | "nav"
+  | "noscript"
+  | "object"
+  | "ol"
+  | "optgroup"
+  | "option"
+  | "output"
+  | "p"
+  | "picture"
+  | "pre"
+  | "progress"
+  | "q"
+  | "rp"
+  | "rt"
+  | "ruby"
+  | "s"
+  | "samp"
+  | "search"
+  | "section"
+  | "select"
+  | "slot"
+  | "small"
+  | "source"
+  | "span"
+  | "strong"
+  | "sub"
+  | "summary"
+  | "sup"
+  | "table"
+  | "tbody"
+  | "td"
+  | "template"
+  | "textarea"
+  | "tfoot"
+  | "th"
+  | "thead"
+  | "time"
+  | "tr"
+  | "track"
+  | "u"
+  | "ul"
+  | "var"
+  | "video"
+  | "wbr";
 
 export function Name(name: string): string;
